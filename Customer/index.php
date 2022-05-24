a2fc74430eaf7c143e9ce3c7897189f0220c0c71
Archiroo <90332385+Archiroo@users.noreply.github.com>
2022-05-24 10:27:44 +0700

    Chia file

--- Customer/index.php
+++ Customer/index.php
@@ -1,4 +1,8 @@
 <?php
     include "headerNoLogin.php";
+    include "homeTypeNoLogin.php";
+    include "homeSaleNoLogin.php";
+    include "listHomeNoLogin.php";
+    include "postNoLogin.php";
     include "footer.php";
 ?>
